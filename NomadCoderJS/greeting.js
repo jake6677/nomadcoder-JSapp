cce3ec49ec4e39c0fd63f10f983d8e13bbb8d2ad
jake6677 <108257001+jake6677@users.noreply.github.com>
2023-09-27 00:04:52 +0900

    solve the refresh problem
    final version.

--- NomadCoderJS/greeting.js
+++ NomadCoderJS/greeting.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+const DoForm = document.querySelector("#todo-form");
+const DoList = document.querySelector("#todo-list");
 
 const HIDDEN_CLASSNAME = "hidden"
 const USERNAME_KEY = "username"
@@ -9,6 +11,8 @@ function loginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, loginInput.value);
+    DoForm.classList.remove(HIDDEN_CLASSNAME);
+    DoList.classList.remove(HIDDEN_CLASSNAME);
     paintGreetings();
 }
 
@@ -23,6 +27,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", loginSubmit);
+    DoForm.classList.add(HIDDEN_CLASSNAME);
+    DoList.classList.add(HIDDEN_CLASSNAME);
 } else {
     paintGreetings();
+    DoForm.classList.remove(HIDDEN_CLASSNAME);
+    DoList.classList.remove(HIDDEN_CLASSNAME);
 }
--- NomadCoderJS/hidden.js
+++ NomadCoderJS/hidden.js
@@ -1,8 +1,6 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
-// const toDoForm = document.querySelector("#todo-form");
-// const toDoList = document.querySelector("#todo-list");
 
 const HIDDEN_CLASSNAME = "hidden"
 const USERNAME_KEY = "username"
@@ -11,8 +9,6 @@ function loginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, loginInput.value);
-    // toDoForm.classList.remove(HIDDEN_CLASSNAME);
-    // toDoList.classList.remove(HIDDEN_CLASSNAME);
     paintGreetings();
 }
 
@@ -29,8 +25,4 @@ if (savedUsername === null) {
     loginForm.addEventListener("submit", loginSubmit);
 } else {
     paintGreetings();
-    // toDoForm.addEventListener("submit", handleToDoSubmit);
-    // toDoForm.classList.remove(HIDDEN_CLASSNAME);
-    // toDoList.classList.remove(HIDDEN_CLASSNAME);
 }
-//to-do form을 제출할 시에 페이지가 새로고침되는 문제가 발생
